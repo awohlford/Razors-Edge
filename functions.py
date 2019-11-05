d6341b3e49309dc7e653e13a5c2a6507e59c5538
awohlford <34891927+awohlford@users.noreply.github.com>
2019-11-05 12:58:42 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -23,5 +23,38 @@ def get_average_possessions(year):
     total = total + x.pace
   return (total / len(teams))
 
-def get_ncaab_historical_data():
-  
+def get_ncaab_historical_betting_data():
+  data = pd.read_csv('ncaa basketball 2018-19.csv', delimiter=',')
+
+def get_ncaab_advanced_statistivs(year=''):
+  if year == '':
+    url = 'https://kenpom.com/index.php?y=' + year
+    page = requests.get(url)
+  else:
+    page = requests.get('https://kenpom.com/index.php')
+  pagetext = page.text
+  soup = BeautifulSoup(pagetext, 'html.parser')
+  temp_stats = []
+  for row in soup.find_all('tr'):
+    count = 0
+    temp = {}
+    for col in row.find_all('td'):
+      if count == 0:
+        temp['adj_em_rank'] = int(col.text)
+      elif count == 1:
+        temp['team'] = str(col.text)
+      elif count == 4:
+        temp['adj_em'] = float(col.text)
+      elif count == 5:
+        temp['adj_o'] = float(col.text)
+      elif count == 6:
+        temp['adj_o_rank'] = int(col.text)
+      elif count == 7:
+        temp['adj_d'] = float(col.text)
+      elif count == 8:
+        temp['adj_d_rank'] = int(col.text)
+      count = count + 1
+    if len(temp) > 1:
+      temp_stats.append(temp)
+  return pd.DataFrame(temp_stats)
+
