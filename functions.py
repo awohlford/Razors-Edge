6a28275a9d20946f99a7d314c5d52dd95279f0ef
awohlford <34891927+awohlford@users.noreply.github.com>
2019-11-05 12:41:34 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,27 @@
+#### Import Necessary Libraries ####
+import json
+import requests
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import sklearn as skl
+import sportsreference.ncaab.teams as ncaab
+import sportsreference.ncaaf.teams as ncaaf
+import sportsreference.nfl.teams as nfl
+import sportsreference.nba.teams as nba
+import sportsreference.mlb.teams as mlb
+from bs4 import BeautifulSoup
+import requests
+import csv
 
+#### Function Definitions Below ####
+def get_average_possessions(year):
+  teams = ncaab.Teams(year)
+  total = 0
+  for x in teams:
+    total = total + x.pace
+  return (total / len(teams))
+
+def get_ncaab_historical_data():
+  
